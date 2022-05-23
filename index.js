d45efb95195cbf9a4bdf043567f1d4d6036b01dc
Ricardo Augusto <ricardoamgermani@gmail.com>
2022-05-23 07:17:10 -0300

    Atualização #3
    Implementação de seção de abertura e conexões entre as entidades inseridas no espaço

--- index.js
+++ index.js
@@ -1,52 +1,92 @@
 const pi = Math.PI
 
 /* VARIÁVEIS IMPORTANTES */
-let svg = $(".list svg"),
-polyg = $("polygon"),
+let pdiv = $(".list .pdiv"),
 main = $("main"),
 html = $("html")[0],
 style = $("style"),
-//P = new Array([], [], [], [], [], [], []),
 namep = new Array("and", "or", "xor", "nand", "nor", "xnor", "not"),
-pdiv = new Array(
-    "0px 0px, 0px 100px",
-    "",
-    "",
-    "0px 0px, 0px 100px",
-    "",
-    "",
-    ""
-),
-TXT = ["1", "‒"],
+TXT = ["1", "-"],//‒"],
 spc = $(".space"),
+btn = $("input"),
 SVG = spc.find("svg"),
 Items = $(".items"),
 M = $(".message"),
+P = new Array([], [], [], [], [], [], []),
 itemsSlct = [],
 allI = new Array(
     [], //---> Portas
     [], //---> Origem
-    [] //---> Destino
-), /*addORXOR = function(i, t) {
-    const ang = i*pi/180, x = -62 + 79.6*Math.cos(ang), y = 50 - 79.6*Math.sin(ang)
-    hSVG[1] += t + x + "," + y
-    hSVG[2] += t + (x + 20) + "," + y
-},*/ ball = (x, y) => {
+    [], //---> Destino
+    [] //---> Linhas entre portas/origens e portas/destinos
+), /*objItem = new Array(
+    (item) => {
+        return { //---> Portas
+            elem: item,
+            E: [],
+            S: null,
+            funct: []
+        }
+    }, (item) => { //---> Origem
+        return {
+            elem: item,
+            S: null,
+            funct: null
+        }
+    }, (item) => { //---> Destino
+        return {
+            elem: item,
+            E: null,
+            funct: null
+        }
+    }, (item) => { //---> Linhas entre portas/origens e portas/destinos
+        return {
+            elem: item,
+            E: null,
+            S: null
+        }
+    }
+),*/ ball = (x, y) => {
     let b = []
     for(let i = 180; i >= -180; i -= 10) {
         const ang = i*pi/180
         const X = x + 7*Math.cos(ang), Y = y + 7*Math.sin(ang)
         b.push([X, Y])
     } return b
-    //return "<circle fill='url(#G)' cx=" + x + " cy=" + y + " r=7></circle>"
 }, changeP = (S, dX, dY) => {
     const V = new Array(dX, dY)
     S.css("left", V[0])
     S.css("top", V[1])
     return V
-}, functMove = (event, W, H, html, bC, cond, n) => {
+}, newLine = (listP) => {
+    let line = listP[0][0] + "," + listP[0][1]
+    for(let i = 1; i < listP.length; i++) {
+        const actp = listP[i]
+        line += " " + actp[0] + "," + actp[1]
+    } return line
+}, createConex = function(values1, values2, q) {
+    const { x1, y1, w1, h1 } = values1,
+          { x2, y2, w2, h2 } = values2
+    let xn = x1 + w1 - 10,
+        yn = y1 + h1/2,
+        Yn = y2 + h2*q
+    let line = newLine(new Array(
+        [xn, yn],
+        [xn + 30, yn],
+        [xn + 30, Yn],
+        [x2 + 30, Yn]
+    ))
+    const newL = $(document.createElementNS('http://www.w3.org/2000/svg', 'polyline'));
+    newL.attr('points', line);
+    newL.appendTo(SVG)
+    return newL
+}, changeConexFunct = function(elem1, elem2, line) {
+    console.log(elem1, elem2, line)
+}, functMove = (event, e, content, bC, n) => {
+    const W = e.width(),
+    H = e.height()
     if(event.which == 1) {
-        let newI = $(html[0] + " style='opacity: 0.5; position: absolute; z-index: 2'>" + html[1]),
+        let newI = $("<div class='" + e.attr("class") + "' style='opacity: 0.5; position: absolute; z-index: 2'>" + content + "</div>"),
         posit = changeP(newI, event.clientX - W/2, event.clientY - H/2)
         newI.prependTo($(document.body))
         $(window).on("mousemove.functW", (e) => posit = changeP(newI, e.clientX - W/2, e.clientY - H/2));
@@ -64,15 +104,17 @@ allI = new Array(
                 } newI.css("left", newL)
                 newI.css("top", newT)
                 newI.css("opacity", "")
-                let Item
-                if(cond) {
-                    Item = newI.children()
-                    $(newI).css("z-index", "")
-                } else {
-                    Item = newI
-                } let qtd = allI[n].length
-                allI[n].push(newI)
-                Item.on("mousedown", function(Event) {
+                let qtd = allI[n].length
+                let objItem = {
+                    elem: newI
+                }; if(!n) {
+                    Object.assign(objItem, {
+                        funct: [],
+                        E: []
+                    })
+                } allI[n].push(objItem)
+                //allI[n].push(objItem[n](newI))
+                newI.on("mousedown", function(Event) {
                     let Posit,
                     functPX = () => {},
                     functPY = () => {}
@@ -134,8 +176,18 @@ allI = new Array(
                                     }
                                 }); if(found) return;
                             }
+                            const T = parseFloat(newI.css("top")),
+                            L = parseFloat(newI.css("left")),
+                            fract = 1/(itemsSlct.length + 1)
                             $.each(itemsSlct, (I, E) => {
-                                allI[E[0]][E[1]].css("filter", "")
+                                const elem = allI[E[0]][E[1]].elem,
+                                t = parseFloat(elem.css("top")),
+                                l = parseFloat(elem.css("left")),
+                                w = elem.width(),
+                                h = elem.height()
+                                elem.css("filter", "")
+                                let polyLine = createConex({ x1: l, y1: t, w1: w, h1: h }, { x2: L, y2: T, w2: W, h2: H }, (I + 1)*fract)
+                                changeConexFunct(elem, newI, polyLine)
                             }); itemsSlct = new Array()
                         } else if(!alt && n !== 2) { //---> Selecionar o início (NÃO pode ser destino)
                             let condNotSlct = true
@@ -151,9 +203,7 @@ allI = new Array(
                                 itemsSlct.push([n, qtd])
                             }
                         }
-                    } /*else if(Event.which == 3) {
-                        newI.remove()
-                    }*/
+                    }
                 })
             } else {
                 newI.remove()
@@ -174,9 +224,10 @@ allI = new Array(
     false, //---> Ctrl + Clique
     false, //---> Ctrl + Alt + Clique (b. esquerdo)
     false //---> Shift + Clique (b. esquerdo): Mude a carga da origem
-], setM
+], setM,
+newStyle = ":root {"
 
-/*$(window).on("keydown", event => {
+$(window).on("keydown", event => {
     const alt = event.altKey,
     ctrl = event.ctrlKey,
     shift = event.shiftKey
@@ -192,19 +243,16 @@ allI = new Array(
         condM[2] = true
         functShowM("<b>Shift + Clique (b. esquerdo)</b>: Mude a carga da origem")
     }
-})*/
+})
+
+/* CRIANDO O FORMATO DAS PORTAS */
 
 // Porta AND e NAND
-//P[0] = P[0].concat([ [0,0], [0,100] ])
+P[0] = new Array([0, 0], [0, 100])
 for(let i = -90; i <= 90; i++) {
     const ang = i*pi/180
-    pdiv[0] += ", " + (50 + 50*Math.cos(ang)) + "px " + (50 - 50*Math.sin(ang)) + "px"
-} style.html(`
-    :root {
-        --and: ` + pdiv[0] + `
-    }`)
-
-P[3] = P[0].slice(0, 92).concat(ball(107, 50), P[0].slice(94, P[0].length))
+    P[0].push([50 + 50*Math.cos(ang), 50 - 50*Math.sin(ang)])
+} P[3] = P[0].slice(0, 92).concat(ball(107, 50), P[0].slice(94, P[0].length))
 
 // Porta OR e XOR
 for(let i = 38; i >= -38; i--) {
@@ -253,19 +301,27 @@ P[6] = new Array([0, 70]).concat(ball(97, 35), new Array([0, 0]))
 
 /* FUNÇÕES DE MOVIMENTO PARA OS ITENS DO MENU */
 
-$.each(svg, (I, E) => {
-    const e = $(E), W = e.width(), H = e.height()
-    $.each(P[I], (i, elem) => {
-        const newp = svg[0].createSVGPoint()
-        newp.x = elem[0]
-        newp.y = elem[1]
-        polyg[I].points.appendItem(newp)
-    })
+$.each(pdiv, (I, E) => {
+    const e = $(E),
+    actp = P[I],
+    Name = namep[I]
+    newStyle += "--" + Name + ":" + actp[0][0] + "px " + actp[0][1] + "px"
+    for(let i = 1; i < actp.length; i++) {
+        newStyle += ", " + actp[i][0] + "px " + actp[i][1] + "px"
+    } newStyle += ";"
     /* Inserir pontos */
-    e.find("use").on("mousedown", (event) => functMove(event, W, H, ["<svg width=" + W + " height=" + H, "<use href=#p" + I + "></use></svg>"], "brightness(0.75) contrast(1.3)", true, 0))
-})
+    e.on("mousedown", (event) => functMove(event, e, "<p>" + Name.toUpperCase() + "</p>", "brightness(0.75) contrast(1.3)", 0))
+}); style.html(newStyle)
 
 $.each(Items, (I, E) => {
     const e = $(E)
-    e.on("mousedown", (event) => functMove(event, e.width(), e.height(), ["<div class='" + e.attr("class") + "'", TXT[I] + "</div>"], "brightness(0.6) contrast(1.6)", false, I + 1))
+    e.on("mousedown", (event) => functMove(event, e, TXT[I], "brightness(0.6) contrast(1.6)", I + 1))
+})
+
+/* BOTÃO NA ENTRADA */
+btn.on("click", function() {
+    $(".introd").css("opacity", 0)
+    setTimeout(function() {
+        $(".introd").remove()
+    }, 300)
 })
